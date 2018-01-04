68a219d308bb614ae819759b4a629fb4c6f20b2d
Austin Green <agreen@zendesk.com>
2018-01-03 16:11:41 -0800

    Removed old styleguide

--- v2/packages/styleguide/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Content } from '@zendeskgarden/react-chrome';
-
-const HomePage = () => <Content>
-    <div>home page content</div>
-</Content>;
-
-export default HomePage;
