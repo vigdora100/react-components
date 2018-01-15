1399139fd95f0635b3af88fd5e8f5db7369f23fd
Austin Green <agreen@zendesk.com>
2018-01-15 14:34:19 -0800

    Added travis publish

--- utils/scripts/publish-updated.js
+++ utils/scripts/publish-updated.js
@@ -1,16 +1,16 @@
-// const childProcess = require('child_process');
-// const path = require('path');
+const childProcess = require('child_process');
+const path = require('path');
 
 
-// const tagString = childProcess
-//   .execSync('git tag -l --contains HEAD')
-//   .toString().trim();
+const tagString = childProcess
+  .execSync('git tag -l --contains HEAD')
+  .toString().trim();
 
-// const updatedPackages = tagString.split('\n').map(tagString => tagString.split('@')[0]);
+const updatedPackages = tagString.split('\n').map(tagString => tagString.split('@')[0]);
 
-// const lernaPath = path.resolve(__dirname, '..', '..', 'node_modules', '.bin', 'lerna');
-// const lernaPackageScope = updatedPackages.join(',');
+const lernaPath = path.resolve(__dirname, '..', '..', 'node_modules', '.bin', 'lerna');
+const lernaPackagesScope = updatedPackages.join(',');
+console.log(lernaPackagesScope);
 
-// // childProcess.execSync(`${lernaPath} exec -- npm publish dist --scope ${lernaPackageScope}`);
+childProcess.execSync(`${lernaPath} exec --bail=<boolean> --scope ${lernaPackagesScope} -- npm publish dist`);
 
-// console.log(lernaPackageScope);
