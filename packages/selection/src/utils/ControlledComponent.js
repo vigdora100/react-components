b8ee5bf4f6672daea6c02b5b39c19ce1a595c407
Austin Green <agreen@zendesk.com>
2018-01-16 16:45:37 -0800

    refactor: add test console.log
    affects: garden-react-selection

--- packages/selection/src/utils/ControlledComponent.js
+++ packages/selection/src/utils/ControlledComponent.js
@@ -1,5 +1,6 @@
 import { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+console.log('test');
 
 /** @component */
 export default class ControlledComponent extends PureComponent {
