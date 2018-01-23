931d3d69844432e11f9b69fb838a573569f8e03f
Austin Green <agreen@zendesk.com>
2018-01-23 22:23:18 +0000

    Updated publish script

--- publish-gh-pages.js
+++ publish-gh-pages.js
@@ -6,12 +6,12 @@ const ghPages = require('gh-pages');
  * This configuration will avoid logging the Github token if there is an error.
  */
 ghPages.publish('styleguide', {
-  repo: `https://${process.env.GITHUB_TOKEN}@github.com:zendeskgarden/react-components.git`,
+  repo: `https://${process.env.GITHUB_TOKEN}@github.com/zendeskgarden/react-components.git`,
   silent: true,
   add: true
 }, (error) => {
   if (error) {
-    console.log(error);
+    throw error;
   }
 
   console.log('GH-Pages upload complete.')
